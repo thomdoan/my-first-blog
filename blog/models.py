2ced163139228901ee756bc41d51cab675a47095
Thomas <thomasjdoan@gmail.com>
2016-01-21 20:12:32 -0600

    Authentication Models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
 
 
 class Post(models.Model):
@@ -17,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
